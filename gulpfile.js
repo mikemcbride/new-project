c99119ca0cba376d44b7777611b0f42d9c5c691e
Mike McBride <mike.mcbride@wwt.com>
2015-07-21 23:30:15 -0500

    Adds images to Gulpfile, other minor enhancements

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var gulp = require('gulp'),
     less = require('gulp-less'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
     gutil = require('gulp-util'),
     watch = require('gulp-watch'),
     rename = require('gulp-rename'),
@@ -14,6 +16,8 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     plumber = require('gulp-plumber'),
     bower = require('main-bower-files');
+
+var bowerDir = './bower_components';
 
 gulp.task('browser-sync', function() {
   browserSync({
@@ -25,9 +29,17 @@ gulp.task('browser-sync', function() {
 
 gulp.task('bs-reload', function() {
   browserSync.reload();
-})
+});
+
+// bring in icon fonts
+gulp.task('icons', function() { 
+  return gulp.src(bowerDir + '/components-font-awesome/fonts/**.*') 
+    .pipe(gulp.dest('dist/fonts')); 
+});
 
 // compile LESS to CSS
+// run autoprefixer
+// minify compiled CSS
 gulp.task('compile-less', function() {
   return gulp.src('src/css/main.less')
     .pipe(plumber())
@@ -38,6 +50,19 @@ gulp.task('compile-less', function() {
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
     .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// compile any third-party libraries into one JS file.
+// includes jQuery by default
+gulp.task('third-party', function() {
+  return gulp.src(bowerDir + '/jquery/dist/jquery.js')
+    .pipe(plumber())
+    .pipe(concat('third-party.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
     .pipe(browserSync.reload({stream: true}));
 });
 
@@ -58,6 +83,18 @@ gulp.task('html', function() {
   return gulp.src('src/*.html')
     .pipe(plumber())
     .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// compress and copy images
+gulp.task('images', function () {
+  return gulp.src('src/img/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('dist/img'))
     .pipe(browserSync.reload({stream: true}));
 });
 
@@ -83,8 +120,9 @@ gulp.task('watch', function() {
   gulp.watch('src/css/*.less', ['compile-less']);
   gulp.watch('src/css/main.css', ['bs-reload']);
   gulp.watch('src/*.html', ['html']);
+  gulp.watch('src/img/*', ['images']);
   gulp.watch('src/js/*.js', ['scripts']);
-});
+})
 
 // cleans dist, builds the files, starts a server, and watches files for changes
 gulp.task('serve', ['clean', 'build', 'browser-sync', 'watch']);
